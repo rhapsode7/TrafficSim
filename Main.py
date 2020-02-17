1050cc6997a6544c82b934cb1c2177cf5887ce5f
Chenjun Tang <54779810+rhapsode7@users.noreply.github.com>
2020-02-16 20:44:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,13 +11,18 @@ def main():
     # initialize an intersection
     intersection = FourWayIntersection(0)
     vid = 0
+    
+    timestamps = s = np.random.poisson(5, 20)
+    timestamps.sort()
 
-    for T in range(1,10,2):
+#     for T in range(1,10,2):
+    for T in timestamps[:10]:
         intersection.enterIntersectionFromLaneID(T, BasicVehicle(vid, intention.LEFT), 0)
         vid += 1
         intersection.enterIntersectionFromLaneID(T, BasicVehicle(vid, intention.STRAIGHT), 1)
         vid += 1
-    for T in range(10,20,2):
+#     for T in range(10,20,2):
+    for T in timestamps[10:]:
         intersection.enterIntersectionFromLaneID(T, BasicVehicle(vid, intention.STRAIGHT), 3)
         vid += 1
         intersection.enterIntersectionFromLaneID(T, BasicVehicle(vid, intention.STRAIGHT), 1)
